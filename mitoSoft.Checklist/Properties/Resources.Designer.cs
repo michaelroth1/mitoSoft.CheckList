9ef3ebc290ef63c4961ee2f9406a3a778c4ed4a2
Michael <michael.mr.roth@live.com>
2026-03-23 20:16:35 +0100

    * Refactor: add icons

--- mitoSoft.Checklist/Properties/Resources.Designer.cs
+++ mitoSoft.Checklist/Properties/Resources.Designer.cs
@@ -61,6 +61,16 @@ namespace mitoSoft.Checklist.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        internal static byte[] Icon {
+            get {
+                object obj = ResourceManager.GetObject("Icon", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap WVE {
